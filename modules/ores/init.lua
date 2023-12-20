afcff0b5165126548cb4a3374930e4549795f6f8
James David Clarke <james@jamesdavidclarke.com>
2023-12-20 15:50:05 +0000

    Fixed Ore Generation

--- modules/ores/init.lua
+++ modules/ores/init.lua
@@ -1,6 +1,22 @@
 -- ores/init.lua
 
 local me = microexpansion
+local mcl_core_modpath = minetest.get_modpath("mcl_core")
+stone_ingrediant = mcl_core_modpath and "mcl_core:stone" or "default:stone"
+
+
+local incranium_y_min = -300
+local incranium_y_max = -90
+
+local quartz_y_min = -31000
+local quartz_y_max = -5
+
+if mcl_core_modpath then
+	incranium_y_min = -55
+	incranium_y_max = -20
+	quartz_y_min = -50
+	quartz_y_max = 0
+end
 
 -- [register] Incranium Ore
 me.register_node("incranium", {
@@ -12,12 +28,12 @@ me.register_node("incranium", {
 	oredef = {
 		{
 			ore_type = "blob",
-			wherein = "default:stone",
+			wherein = stone_ingrediant,
 			clust_scarcity = 4*4*4,
 			clust_num_ores = 4,
 			clust_size = 3,
-			y_min = -300,
-			y_max = -90,
+			y_min = incranium_y_min,
+			y_max = incranium_y_max,
 		},
 	},
 	disabled = true,
@@ -37,11 +53,11 @@ me.register_node("quartz", {
 	drop = "microexpansion:quartz_crystal",
 	oredef = {{
 		ore_type = "scatter",
-		wherein = "default:stone",
+		wherein = stone_ingrediant,
 		clust_scarcity = 10*10*10,
 		clust_num_ores = 6,
 		clust_size = 5,
-		y_min = -31000,
-		y_max = -5,
+		y_min = quartz_y_min,
+		y_max = quartz_y_max,
 	}}
 })
