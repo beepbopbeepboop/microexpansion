8176d6fa533ff3cb0d65246deccab6e611f3f074
Mike Stump <mikestump@comcast.net>
2023-12-27 14:51:01 -0800

    Fix formatting in alternatives.lua.

--- modules/crafting/alternatives.lua
+++ modules/crafting/alternatives.lua
@@ -10,28 +10,28 @@ local me = microexpansion
 -- a different logic chip that uses gold, quartz and wood
 -- for use instead of basic_materials:ic that requires sand, coal and copper
 me.register_item("logic_chip", {
-	description = "Control Unit",
-	recipe = {
-		{ 2,
-			{
-				{"basic_materials:gold_wire"},
-				{"basic_materials:silicon"},
-				{"basic_materials:plastic_sheet"}
-			},
-		},
-		{ 2,
-			{
-				{"basic_materials:gold_wire"},
-				{"microexpansion:quartz_crystal"},
-				{"basic_materials:plastic_sheet"}
-			},
-		},
-		{ 2, 
-			{
-				{"microexpansion:gold_wire"},
-				{"microexpansion:quartz_crystal"},
-				{"group:wood"}
-			},
-		},
-	},
+  description = "Control Unit",
+  recipe = {
+    { 2,
+      {
+        {"basic_materials:gold_wire"},
+        {"basic_materials:silicon"},
+        {"basic_materials:plastic_sheet"}
+      },
+    },
+    { 2,
+      {
+        {"basic_materials:gold_wire"},
+        {"microexpansion:quartz_crystal"},
+        {"basic_materials:plastic_sheet"}
+      },
+    },
+    { 2, 
+      {
+        {"microexpansion:gold_wire"},
+        {"microexpansion:quartz_crystal"},
+        {"group:wood"}
+      },
+    },
+  },
 })
